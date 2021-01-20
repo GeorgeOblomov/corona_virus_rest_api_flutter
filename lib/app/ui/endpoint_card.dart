a12a81463d232f475970b8183bba8beb1fa0e6b9
GeorgeOblomov <grischenkov01@gmail.com>
2021-01-21 01:12:06 +0300

    2021-01-20 v. 1.0.0 Dev: Adding icons & adding color to widgets

--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,43 +1,73 @@
 import 'package:corona_virus_rest_api_flutter_courses/app/services/api.dart';
 import 'package:flutter/material.dart';
 
+class EndpointCardData {
+  final String title;
+  final String assetName;
+  final Color color;
+
+  EndpointCardData(this.title, this.assetName, this.color);
+}
+
 class EndpointCard extends StatelessWidget {
   final Endpoint endpoint;
   final int value;
 
-  static Map<Endpoint, String> _cardTitles = {
-    Endpoint.cases: 'Cases',
-    Endpoint.casesConfirmed: 'Confirmed cases',
-    Endpoint.casesSuspected: 'Suspected cases',
-    Endpoint.deaths: 'Deaths',
-    Endpoint.recovered: 'Recovered'
+  static Map<Endpoint, EndpointCardData> _cardData = {
+    Endpoint.cases:
+        EndpointCardData('Cases', 'assets/count.png', Color(0xFFFFF492)),
+    Endpoint.casesConfirmed: EndpointCardData(
+        'Confirmed cases', 'assets/fever.png', Color(0xFFE99600)),
+    Endpoint.casesSuspected: EndpointCardData(
+        'Suspected cases', 'assets/suspect.png', Color(0xFFEEDA28)),
+    Endpoint.deaths:
+        EndpointCardData('Deaths', 'assets/death.png', Color(0xFFE40000)),
+    Endpoint.recovered:
+        EndpointCardData('Recovered', 'assets/patient.png', Color(0xFF70A901))
   };
   //TODO: Make possible to prevent overflow by 18 pixels on the right
   EndpointCard({Key key, this.endpoint, this.value});
   @override
   Widget build(BuildContext context) {
+    final cardData = _cardData[endpoint];
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width / 3,
-                child: Text(
-                  _cardTitles[endpoint],
-                  style: Theme.of(context).textTheme.headline5,
-                  maxLines: 2,
+              Text(
+                cardData.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5
+                    .copyWith(color: cardData.color),
+                maxLines: 2,
+              ),
+              SizedBox(height: 4),
+              SizedBox(
+                height: 52,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(cardData.assetName, color: cardData.color),
+                    (value != null)
+                        ? Text(
+                            value != null ? value.toString() : '',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline4
+                                .copyWith(
+                                    color: cardData.color,
+                                    fontWeight: FontWeight.w500),
+                          )
+                        : CircularProgressIndicator(),
+                  ],
                 ),
               ),
-              (value != null)
-                  ? Text(
-                      value != null ? value.toString() : '',
-                      style: Theme.of(context).textTheme.headline4,
-                    )
-                  : CircularProgressIndicator(),
             ],
           ),
         ),
